c05204dff4a300b531a1ccfbb285d2245e4ac73a
kstepanyan111 <kstepananyan@gmail.com>
2022-02-02 15:50:57 +0400

    Updated configs

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,14 @@ import { IConfig } from "./interfaces/IConfig"
 
 export default {
     redisUrl: 'redis://127.0.0.1:6379',
-    discordToken: 'OTM3Njg5OTY3OTE0Nzk1MDg5.YffZ1Q.MEkOkm0BeJ3M_YTfD8ZpqHgvfjA',
+    discordToken: "<bot_token>",
     prefix: '!',
-    notifyChannel: '937692634003157046'
+    notifyChannel: '<bot_channel>'
 }
 
 export const defaultConfig: IConfig = {
     cycleTime: 60,
     notifyCycleTime: 20,
-    nodes: {'test': '143.110.246.141:26657'},
-    operators: [],
-    n_peers: 40
+    nodes: {},
+    operators: []
 }
